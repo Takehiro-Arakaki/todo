fbcbe0762d3b13e3a7518c2cbffba1abdcf776f8
arakaki <takehiroarakaki@gmail.com>
2020-11-24 08:41:34 +0900

    first commit

---
+++ todo-web/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="user">
+    <p>{{user.name}}</p>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="user.todos" />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
+import axios from "@/plugins/axios";
+
+export default {
+  fetch({ store, redirect }) {
+    store.watch(
+      state => state.currentUser,
+      (newUser, oldUser) => {
+        if (!newUser) {
+          return redirect("/login");
+        }
+      }
+    );
+  },
+  components: {
+    AddTodo,
+    TodoList
+  },
+  computed: {
+    user() {
+      return this.$store.state.currentUser;
+    }
+  },
+  methods: {
+    async addTodo(todo) {
+      const { data } = await axios.post("/v1/todos", { todo });
+      this.$store.commit("setUser", {
+        ...this.user,
+        todos: [...this.user.todos, data]
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ todo-web/store/state.js
@@ -0,0 +1,10 @@
+const state = {
+  currentUser: {},
+  loading: false,
+  notification: {
+      status: false,
+      message: ""
+  }
+}
+
+export default state
